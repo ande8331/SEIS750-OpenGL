73306b153d0e2ab6ff0ba74ca6e86a8f90e4add9
Ross Anderson <ande8331@stthomas.edu>
2012-03-06 19:54:56 -0600

    Have a green ball.

---
+++ Week6Demo/vshader-lighting.glsl
@@ -0,0 +1,29 @@
+#version 150
+
+in vec4 vPosition;
+in vec4 vAmbientDiffuseColor;
+in vec3 vNormal; // must be in position 3 for teapot
+uniform mat4 model_view;
+uniform mat4 projection;
+uniform vec4 ambient_light;
+uniform vec4 light_position;
+uniform vec4 light_color;
+out vec4 color;
+
+void
+main()
+{
+	vec4 veyepos = model_view*vPosition;
+	vec4 normal = vec4(vNormal, 0);		// Converts the vec3 to a vec4
+	vec3 L = normalize( light_position.xyz - veyepos.xyz ); // use vec3 because it works better in a dot product
+	// above line is a vector
+	// .xxxx is a vec4 of x in each position (swizziling)
+
+	vec3 N = normalize(( model_view * normal).xyz);  // apply any mv transformations to the normal (needed to be vec4 to allow 4x4 to multiply it
+
+
+    gl_Position = projection*veyepos;
+	color = ambient_light*vAmbientDiffuseColor
+	+
+	light_color * vAmbientDiffuseColor * max(0.0, dot(L, N));
+}
