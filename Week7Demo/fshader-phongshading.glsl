829f8a6664c3f9490c72edb485e996c7232d9df2
Ross Anderson <ande8331@stthomas.edu>
2012-03-13 19:24:12 -0500

    Corrected normalizations.

--- Week7Demo/fshader-phongshading.glsl
+++ Week7Demo/fshader-phongshading.glsl
@@ -2,16 +2,20 @@
 uniform vec4 ambient_light;
 in vec4 fvAmbientDiffuseColor;
 uniform vec4 light_color;
+uniform vec4 light_position;
 in float fvSpecularExponent;
 in vec4 fvSpecularColor;
-in vec3 N;
-in vec3 V;
-in vec3 H;
-in vec3 L;
+in vec3 vN;
+in vec4 position;
 out vec4  fColor;
 
 void main()
 {    
+	vec3 L = normalize(light_position.xyz -position.xyz);
+	vec3 E = normalize (-position.xyz);
+	vec3 N = normalize (vN);
+	vec3 H = normalize (L+E);
+
 	vec4 ambient = ambient_light*fvAmbientDiffuseColor;
 	vec4 diffuse = light_color * fvAmbientDiffuseColor * max(0.0, dot(L, N));
 	vec4 specular = light_color * fvSpecularColor * pow(max(0.0, dot(N, H)), fvSpecularExponent); // Specular
