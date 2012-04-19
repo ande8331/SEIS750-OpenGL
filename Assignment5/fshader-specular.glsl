7e18fe82dc7a319cab146d3e27e59e1d0530260a
Ross Anderson <ande8331@stthomas.edu>
2012-04-18 20:06:12 -0500

    Blending day and night.

--- Assignment5/fshader-specular.glsl
+++ Assignment5/fshader-specular.glsl
@@ -30,9 +30,19 @@ void main()
 	vec4 diffuse = light_color * tmpfvAmbientDiffuseColor * max(0.0, dot(L, N));
 	vec4 specular = light_color * specMap * pow(max(0.0, dot(N, H)), specMap.w*200); // Specular
 	
-	if (dot (L, N) < 0)
+	if (dot (L, N) < 0.0)
 	{
+		ambient = texture2D(nightTexture, fTexCoord);
+		diffuse = vec4(0,0,0,1);
 		specular = vec4(0, 0, 0, 1); // Certain positionings cause problems (being on wrong side of object), test for it, throw it out
+	}
+	else if (dot (L,N) < 0.20)
+	{
+		float dayAverage = (dot(L,N)/0.20);
+		float nightAverage = 1 - dayAverage;
+		ambient = (ambient*dayAverage) + (texture2D(nightTexture, fTexCoord) * nightAverage);
+		diffuse = (diffuse*dayAverage);
+		specular = (specular*dayAverage);
 	}
 
 	fColor = ambient + diffuse + specular;
